f39dd94bae4c9024e82efcbe687eae7eb162330a
Alexandre Callens <alekzxand@gmail.com>
2017-12-04 13:18:37 +0400

    admin

--- config/routes.js
+++ config/routes.js
@@ -23,11 +23,31 @@ module.exports = function (app, passport) {
         failureFlash: true // allow flash messages
     }));
     // process the login form
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/home', // redirect to the secure profile section
-        failureRedirect: '/login', // redirect back to the signup page if there is an error
-        failureFlash: true // allow flash messages
-    }));
+    app.post('/login', (req, res) => {
+        //Redirect user according to role
+        passport.authenticate('local-login', (err, user, info) => {
+                if (err) {
+                    return res.redirect('/login');
+                }
+                if (!user) {
+                    return res.redirect('/login');
+                }
+                //Log in the user
+                req.logIn(user, (err) => {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    //redirect the user to dashboard when it's an admin
+                    if (user.local.role === 'admin') {
+                        return res.redirect('/dashbord');
+                    }
+                    //redirect user to the homepage for no admin user
+                    return res.redirect('/');
+                });
+            })
+            (req, res); //<-- give access to req and res for the callback of authenticate
+    });
 
 
 }
