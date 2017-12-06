b44daed29339db272348bd2251459ceb97fe0b93
Alexandre Callens <alekzxand@gmail.com>
2017-12-06 14:31:32 +0400

    ok

--- app/controllers/articleController.js
+++ app/controllers/articleController.js
@@ -25,10 +25,7 @@ class articleController{
         } else {
             img_path = req.body.img;
             console.log('defini en tant qu image')
-        }
-      
-        
-        
+        }     
         let myData = new Article({
            title : req.body.title,
            preview : req.body.preview,
@@ -37,7 +34,6 @@ class articleController{
            date : dateFormat,
            brouillon: false
         });
-      
         
         myData.save()
         .then(item => {
@@ -68,8 +64,6 @@ class articleController{
             img_path = req.body.img;
             console.log('defini en tant qu image')
         }
-
-
        let myData = new Article({
             title: req.body.title,
             preview : req.body.preview,
@@ -110,89 +104,47 @@ class articleController{
             }) 
         }
 
-
-  
-
-
-
-
-
-
-   /* edit(req ,res){
-        let fileToUpload = req.file;
-   
-
-        if (fileToUpload != undefined || fileToUpload != null) {
-            target_path = 'public/images/' + fileToUpload.originalname;
-            tmp_path = fileToUpload.path;
-            img_path = fileToUpload.originalname;
-        } else {
-             img_path = req.body.img;
-              console.log('defini en tant qu image : ' + img_path)
-        }
-
-        let article = {
-            img : img_path,
-            title: req.body.title,
-            preview : req.body.preview,
-            content : req.body.content,
-            date : dateFormat,
-            brouillon: req.body.brouillon
-        };
-            
-
-        Article.findByIdAndUpdate({_id:req.params.id}, article, () => {
+        edit(req, res) {
+            let fileToUpload = req.file;
+            let target_path;
+            let tmp_path;
+            let img_path;
             if (fileToUpload != undefined || fileToUpload != null) {
-                let src = fs.createReadStream(tmp_path);
-                let dest = fs.createWriteStream(target_path);
-                src.pipe(dest);
-
-                fs.unlink(tmp_path);
-                console.log("ça marche pas mec");
-            }else{
-            res.redirect('/admin/liste-articles/');
+                target_path = 'public/images/' + fileToUpload.originalname;
+                tmp_path = fileToUpload.path;
+                img_path = fileToUpload.originalname;
+            } else {
+                img_path = req.body.img;
             }
-        })
-    }*/
 
-    edit(req, res){
-        let fileToUpload = req.file;
-        let target_path;
-        let tmp_path;
-        let img_path;
-        if (fileToUpload != undefined || fileToUpload != null) {
-            target_path = 'public/images/' + fileToUpload.originalname;
-            tmp_path = fileToUpload.path;
-            img_path = fileToUpload.originalname;
-        } else {
-            img_path = req.body.img;
-        }
-        
-        Article.findByIdAndUpdate(req.params.id, {
-            $set:{
-                title: req.body.title,
-                preview: req.body.preview,
-                content: req.body.content,
-                img: img_path
-            }
-        }, {
-            new: true
-        }, (err, article) => {
-            
-                if (fileToUpload != undefined || fileToUpload != null) {
-                    let src = fs.createReadStream(tmp_path);
-                    let dest = fs.createWriteStream(target_path);
-                    src.pipe(dest);
-    
-                    fs.unlink(tmp_path);
-                    console.log("ça marche pas mec");
-                }else{
-                res.redirect('/admin/liste-articles/');
+            Article.findByIdAndUpdate(req.params.id, {
+                $set: {
+                    title: req.body.title,
+                    preview: req.body.preview,
+                    content: req.body.content,
+                    img: img_path
                 }
-           
-        } 
-    )
-    }
+            }, {
+                    new: true
+                }, (err, article) => {
+                    article.save().then(item => {
+    
+                        if (fileToUpload != undefined || fileToUpload != null) {
+                            let src = fs.createReadStream(tmp_path);
+                            let dest = fs.createWriteStream(target_path);
+                            src.pipe(dest);
+                 
+                            fs.unlink(tmp_path);
+                            console.log('Ca marche toujours')
+                        }
+                        res.redirect('/admin/liste-articles')
+                    })
+                        .catch(err => {
+                            res.status(400);
+                        });
+                }
+            )
+        }
 
 
     draftToArticle(req, res){
