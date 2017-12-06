c013d6cf2c2300b6838b5bd391368ad3d09cffd0
hiromeister <hermandrobin@gmail.com>
2017-12-06 14:05:01 +0400

    ALERT BROUILLON

--- app/controllers/articleController.js
+++ app/controllers/articleController.js
@@ -4,11 +4,10 @@ const dateFormat = require('dateformat');
 const Article = require('../models/article');
 var multer = require('multer')
 const fs = require('fs')
+
 let target_path;
 let tmp_path;
 let img_path;
-
-
 class articleController{
     
     create(req, res){
@@ -117,29 +116,32 @@ class articleController{
 
 
 
-    /**edit(req, res){
+
+
+   /* edit(req ,res){
         let fileToUpload = req.file;
-        
-                if (fileToUpload != undefined || fileToUpload != null) {
-                    target_path = 'public/images/' + fileToUpload.originalname;
-                    tmp_path = fileToUpload.path;
-                    img_path = fileToUpload.originalname;
-                    console.log('pas defini')
-                } else {
-                    img_path = req.body.img;
-                    console.log('defini en tant qu image')
-                }
-                
-        let myData = new Article({
+   
+
+        if (fileToUpload != undefined || fileToUpload != null) {
+            target_path = 'public/images/' + fileToUpload.originalname;
+            tmp_path = fileToUpload.path;
+            img_path = fileToUpload.originalname;
+        } else {
+             img_path = req.body.img;
+              console.log('defini en tant qu image : ' + img_path)
+        }
+
+        let article = {
+            img : img_path,
             title: req.body.title,
             preview : req.body.preview,
             content : req.body.content,
-            img : img_path,
             date : dateFormat,
-            brouillon: true 
-        })
+            brouillon: req.body.brouillon
+        };
+            
 
-        Article.findByIdAndUpdate({_id:req.params.id}, myData, () =>{
+        Article.findByIdAndUpdate({_id:req.params.id}, article, () => {
             if (fileToUpload != undefined || fileToUpload != null) {
                 let src = fs.createReadStream(tmp_path);
                 let dest = fs.createWriteStream(target_path);
@@ -147,23 +149,54 @@ class articleController{
 
                 fs.unlink(tmp_path);
                 console.log("ça marche pas mec");
-            }else {
-            res.redirect('/admin/liste-articles/'); 
-            console.log("GG");
+            }else{
+            res.redirect('/admin/liste-articles/');
             }
         })
-    }
-*/
+    }*/
 
-    edit(req ,res){
-        let article = req.body;
-        Article.findByIdAndUpdate({_id:req.params.id}, article, () => {
-            res.redirect('/admin/liste-articles/');
-        })
+    edit(req, res){
+        let fileToUpload = req.file;
+        let target_path;
+        let tmp_path;
+        let img_path;
+        if (fileToUpload != undefined || fileToUpload != null) {
+            target_path = 'public/images/' + fileToUpload.originalname;
+            tmp_path = fileToUpload.path;
+            img_path = fileToUpload.originalname;
+        } else {
+            img_path = req.body.img;
+        }
+        
+        Article.findByIdAndUpdate(req.params.id, {
+            $set:{
+                title: req.body.title,
+                preview: req.body.preview,
+                content: req.body.content,
+                img: img_path
+            }
+        }, {
+            new: true
+        }, (err, article) => {
+            
+                if (fileToUpload != undefined || fileToUpload != null) {
+                    let src = fs.createReadStream(tmp_path);
+                    let dest = fs.createWriteStream(target_path);
+                    src.pipe(dest);
+    
+                    fs.unlink(tmp_path);
+                    console.log("ça marche pas mec");
+                }else{
+                res.redirect('/admin/liste-articles/');
+                }
+           
+        } 
+    )
     }
+
 
     draftToArticle(req, res){
-        let article = {
+        let article = {     
             title: req.body.title,
             preview : req.body.preview,
             content : req.body.content,
