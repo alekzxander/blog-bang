f3d513cf61ebf997cdba40614fcf5147b32b96d2
hiromeister <hermandrobin@gmail.com>
2017-12-07 14:20:29 +0400

    oula

--- config/database.js
+++ config/database.js
@@ -1,6 +1,13 @@
-module.exports = {
+/* module.exports = {
 
     'url' : 'mongodb://localhost:27017/blog-bang' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
      
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
-};
+}; */ 
+
+const dotenv = require('dotenv').load();
+
+module.exports = {
+    /* 'url' : 'mongodb://localhost:27017/cfdtvote' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot */
+    'url': process.env.MONGODATA
+}
